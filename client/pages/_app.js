a8a84c870ac1e110329b952ccd57e9501a90a214
Morishing362 <morisita611@gmail.com>
2024-11-08 18:34:01 +0900

    client

---
+++ client/pages/_app.js
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.css";
+
+export default ({ Component, pageProps }) => {
+  return (
+    <div>
+      <h1>Header!</h1>
+      <Component {...pageProps} />
+    </div>
+  );
+};
