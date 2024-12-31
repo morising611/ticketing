54e7348aa42f279e38e181647bced01d52fa8d73
Morishing362 <morisita611@gmail.com>
2024-12-31 14:11:53 +0900

    expiration service complete

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,14 +1,7 @@
-import mongoose from 'mongoose';
-import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
-import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
-  if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY must be defined');
-  }
-
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error('NATS_CLIENT_ID must be defined');
   }
@@ -17,10 +10,6 @@ const start = async () => {
   }
   if (!process.env.NATS_CLUSTER_ID) {
     throw new Error('NATS_CLUSTER_ID must be defined');
-  }
-
-  if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined');
   }
 
   try {
@@ -38,17 +27,9 @@ const start = async () => {
     process.on('SIGTERM', () => natsWrapper.client.close());
 
     new OrderCreatedListener(natsWrapper.client).listen();
-    new OrderCancelledListener(natsWrapper.client).listen();
-
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('Mongo DB connected');
   } catch (err) {
     console.log(err);
   }
 };
-
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!!!');
-});
 
 start();
